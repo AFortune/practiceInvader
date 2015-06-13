66b9179379570a7507063048434a9e18892ea743
AFortune <aaron.fortune@icloud.com>
2015-06-13 18:02:58 +0200

    initial and late commit everything working right now except aliens move over too far

---
+++ res/game.js
@@ -0,0 +1,86 @@
+//the actual game logic
+
+var arcadeBox, input, frames, spFrame, lvFrame;
+var alSprite, taSprite, ciSprite;
+var aliens, dir, tank, bullets, cities;
+
+var main = function(){
+    arcadeBox = new ArcadeBox(510, 600);
+    input = new InputHandler();
+
+    var img = new Image();
+    img.addEventListener("load", function(){
+        alSprite = [
+            [new Sprite(this,0,0,22,16),new Sprite(this,0,16,22,16)],
+            [new Sprite(this,22,0,16,16),new Sprite(this,22,16,16,16)],
+            [new Sprite(this,38,0,24,16),new Sprite(this,38,16,24,16)]
+        ];
+        taSprite = new Sprite(this,62,0,22,16);
+        ciSprite = new Sprite(this,64,8,36,24);
+        init();//find out the importance of function dec order
+        run();
+    });
+    img.src = "invaders.png"; 
+};
+var init = function(){
+    frames = 0;
+    dir = 1;
+    spFrame = 0;
+    lvFrame = 50;
+    aliens = [];
+    var rows = [1,0,0,2,2];
+    for(var i=0, len=rows.length; i<len;i++){
+        for(var j=0; j<10;j++){
+           var a = rows[i];
+           aliens.push({//this is crazy go through this slow
+               sprite: alSprite[a],
+               x: 30 + j*30 + [0,4,0][a],
+               y: 30 + i*30,
+               w: alSprite[a][0].w,
+               h: alSprite[a][0].h
+           
+           })
+        }
+    }
+    
+};
+var run = function(){
+    var loop = function(){
+        update();
+        render();
+        window.requestAnimationFrame(loop, arcadeBox.canvas);
+    };
+    window.requestAnimationFrame(loop, arcadeBox.canvas);
+};
+var update = function(){
+    frames++;
+    if(frames % lvFrame === 0){
+        spFrame = (spFrame + 1) % 2;//also what on earth is this?
+    
+        var _max = 1;
+        var _min = arcadeBox.width;
+        for(var i = 0; i<aliens.length; i++){
+            var a = aliens[i];//bug near here somewhere
+            a.x += 30 * dir;
+            _max = Math.max(_max, a.x, a.w);
+            _min = Math.min(_min, a.x);
+        }
+        if(_max > arcadeBox.width || _min < 30){
+            dir = dir * -1;
+            for(var i = 0; i< aliens.length; i++){
+                aliens[i].x += 30*dir;
+                aliens[i].y += 30;
+            }
+
+        }
+    }
+};
+var render = function(){
+    arcadeBox.clear();
+    for(var i = 0; i<aliens.length; i++){
+        var a = aliens[i];
+        arcadeBox.drawSprite(a.sprite[spFrame], a.x,a.y);
+    }
+};
+
+main();//why is this here
---
+++ res/jshelper.js
@@ -0,0 +1,69 @@
+//helper functions
+
+
+
+
+
+//ArcadeBox instead of Screen
+
+var ArcadeBox = function(width, height){
+    this.canvas = document.createElement("canvas");
+    this.width = width;
+    this.height = height;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.ctx = this.canvas.getContext('2d');
+    document.body.appendChild(this.canvas);
+
+};
+
+ArcadeBox.prototype.drawSprite = function(sprite, x, y){
+    this.ctx.drawImage(sprite.img, sprite.x, sprite.y, sprite.w, sprite.h, x, y, sprite.w, sprite.h);
+};
+ArcadeBox.prototype.clear = function(){
+    this.ctx.clearRect(0,0,this.width, this.height);
+};
+
+//Sprite
+
+var Sprite = function(img, x, y, w, h){
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+};
+
+
+//InputHandler
+var InputHandler = function(){
+    this.down = {};
+    this.pressed = {};
+    var self = this;
+    document.addEventListener("keydown", function(evt){
+    
+        self.down = [evt.keyCode] = true;
+    });
+    document.addEventListener("keyup", function(evt){
+        delete self.down[evt.keyCode];
+        delete self.pressed[evt.keyCode];
+    
+    });
+};
+
+InputHandler.prototype.isDown = function(code){
+    return this.down[code];
+};
+
+InputHandler.prototype.isPressed = function(code){//find this really confusing at the moment
+    if(this.pressed[code]){
+        return false;
+    }
+    else if(this.down[code]){
+        return this.pressed[code] = true;
+    }
+    return false;
+};
+
+
+
